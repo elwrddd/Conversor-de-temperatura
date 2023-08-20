e1bbd11ecad16e861654b7aec73a915585d6659d
Eduardo.7 <elwrddd220@outlook.com>
2023-08-20 13:14:28 -0300

    explicações de como o código funciona

--- script.js
+++ script.js
@@ -8,40 +8,40 @@ function converter(){
         window.alert('Verifique os inputs')
     } else{
         let t1 = Number(tempIni.value)
-
+        /* Caso o primeiro select for Celsius */
         if (temp1.selectedIndex == 0){
-
+            /* Caso o segundo select for Fahrenheit */
             if (temp2.selectedIndex == 1){
                 let t2 = t1 * 9/5 + 32
                 res.innerHTML = (`${t2.toFixed(2)}Fº`)
             }
-
+            /* Caso o segundo select for Kelvin */
             if (temp2.selectedIndex == 2){
                 let t2 = t1 + 273.15
                 res.innerHTML = (`${t2.toFixed(2)}K`)
             }
         }
-
+        /* Caso o primeiro select for Fahrenheit */
         if (temp1.selectedIndex == 1){
-
+            /* Caso o segundo select for Celsius */
             if (temp2.selectedIndex == 0){
                 let t2 = (t1 - 32) * 5/9
                 res.innerHTML = (`${t2.toFixed(2)} Cº`)
             }
-
+            /* Caso o segundo select for Kelvin */
             if (temp2.selectedIndex == 2){
                 let t2 = (t1 - 32) * 5/9 + 273.15
                 res.innerHTML = (`${t2.toFixed(2)} K`)
             }
         }
-
+        /* Caso o primeiro select for Kelvin */
         if (temp1.selectedIndex == 2){
-
+            /* Caso o segundo select for Celsius */
             if (temp2.selectedIndex == 0){
                 let t2 = t1 - 273.15
                 res.innerHTML = (`${t2.toFixed(2)} Cº`)
             }
-
+            /* Caso o segundo select for Fahrenheit */
             if (temp2.selectedIndex == 1){
                 let t2 = (t1 - 273.15) * 9/5 + 32 
                 res.innerHTML = (`${t2.toFixed(2)} Fº`)
